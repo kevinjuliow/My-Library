4bb9a51281ea2f2ca915d579286c8ff44aa339b4
kevinjuliow <kevjuliow@gmail.com>
2023-07-26 21:34:33 +0700

    commit

--- client/src/Pages/Add/AddBook.jsx
+++ client/src/Pages/Add/AddBook.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import TextBox from './TextBox'
+import {GrChapterAdd} from 'react-icons/gr'
 
 const AddBook = () => {
   return (
     <div style={{display:'flex' , flexDirection:'column' , alignItems:"center" , marginTop : "5%" }}>
-      Add New Book
+      <p style={{fontSize:"30px"}}>Add New Book</p>
       <TextBox props={"Book Title"}/>
       <TextBox props={"Author"}/>
       <TextBox props={"Publisher"}/> 
       <TextBox props={"Year Release"}/> 
       <TextBox props={"Total Pages"}/> 
+      <div style={{position:"absolute" , right:"10%" ,cursor:"pointer", bottom:'50px' , display:'flex' , flexDirection:"column"}}>
+      <GrChapterAdd style={{fontSize:"50px"}}/>
+      <p style={{fontSize:"20px" , textAlign:"center"}}>Add</p>
+      </div>
+  
     </div>
   )
 }
